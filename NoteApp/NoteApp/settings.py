02cf8ce80908b50e2e5ed9e98a33c78efb50e96c
VICTORIA EKPOH <vickyhail@yahoo.com>
2021-08-15 17:17:22 +0100

    second commit after hidding secretekey

--- NoteApp/NoteApp/settings.py
+++ NoteApp/NoteApp/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-bq2$h&)x^d+p*y4%fqyi8byr6$j&p_5h_ln(x(@pza)i@vj-h('
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
