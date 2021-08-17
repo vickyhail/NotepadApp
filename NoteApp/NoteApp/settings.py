3b8bb3ab2f2d15a3b334d68c2be2d725c78b0514
VICTORIA EKPOH <vickyhail@yahoo.com>
2021-08-17 15:43:55 +0100

    configure .env file for python everywhere

--- NoteApp/NoteApp/settings.py
+++ NoteApp/NoteApp/settings.py
@@ -14,7 +14,9 @@ from pathlib import Path
 import os
 from dotenv import load_dotenv
 
-load_dotenv()
+load_dotenv(
+/home/vickyhail/NotepadApp/NoteApp/NoteApp/.env)
+#load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
