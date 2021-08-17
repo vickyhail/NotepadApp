1a803a0e4871853ade0741e9b8653fd8d3153f65
VICTORIA EKPOH <vickyhail@yahoo.com>
2021-08-17 14:33:18 +0100

    added requiremnts

--- NoteApp/NoteApp/settings.py
+++ NoteApp/NoteApp/settings.py
@@ -125,10 +125,14 @@ import os
 
 
 STATIC_URL = '/static/'
+#STATICFILES_DIRS = [ os.path.join(BASE_DIR, 'static'),]
+
 STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
 
-STATICFILES_DIRS = [ os.path.join(BASE_DIR, 'static'),]
+
+    
+  
 
 
 #STATIC_ROOT = os.path.join(BASE_DIR, 'static')
